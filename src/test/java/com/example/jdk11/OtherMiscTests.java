ecdb2872d8e09bfb8be90ecd86ddbd34909d1f89
Scott Leberknight <scottleber@gmail.com>
2020-03-14 11:28:12 -0400

    Finish the OtherMiscTests
    * Finish test of TimeUnit#convert
    * Finish 'not' Predicate test
    * Add lombok.config to set log fields to LOG (not log)

--- src/test/java/com/example/jdk11/OtherMiscTests.java
+++ src/test/java/com/example/jdk11/OtherMiscTests.java
@@ -1,33 +1,46 @@
 package com.example.jdk11;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import static java.util.function.Predicate.not;
+import static java.util.stream.Collectors.toUnmodifiableSet;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Other misc. JDK 11 new APIs")
+@Slf4j
 class OtherMiscTests {
 
     @Test
     void shouldConvertTimeUnitToDuration() {
-        // TODO
+        var unit = TimeUnit.of(ChronoUnit.HOURS);
+        LOG.debug("TimeUnit: {}", unit);
 
-        TimeUnit unit = TimeUnit.of(ChronoUnit.HOURS);
-        System.out.println("unit = " + unit);
-
-        Duration duration = Duration.ofDays(45);
-        System.out.println("duration = " + duration);
+        var duration = Duration.ofDays(3);
+        LOG.debug("Duration: {}", duration);
 
         long amount = unit.convert(duration);
-        System.out.println("amount = " + amount);
+        LOG.debug("TimeUnit {} amount: {}", unit, amount);
 
-        // TimeUnit.convert
+        assertThat(amount).isEqualTo(72L);
     }
 
     @Test
     void shouldAllowNotPredicate() {
-        // TODO
+        Predicate<String> longerThanThreeChars = str -> str.length() > 3;
+
+        var filteredNames = List.of("bob", "alice", "carlos", "diane")
+                .stream()
+                .filter(not(longerThanThreeChars))
+                .collect(toUnmodifiableSet());
+
+        assertThat(filteredNames).containsExactly("bob");
     }
 }
